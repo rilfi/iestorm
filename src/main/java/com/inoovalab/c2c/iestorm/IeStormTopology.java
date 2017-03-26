4415b576dadf7121c41bf8bfbcb6ad7112f58673
rilfi <rilfimail@gmail.com>
2017-03-26 20:54:19 +0530

    with print matrix

--- src/main/java/com/inoovalab/c2c/iestorm/IeStormTopology.java
+++ src/main/java/com/inoovalab/c2c/iestorm/IeStormTopology.java
@@ -1,4 +1,3 @@
-
 package com.inoovalab.c2c.iestorm;
 
 import com.inoovalab.c2c.iestorm.rich_topology.*;
@@ -20,33 +19,29 @@ import java.util.Map;
 public class IeStormTopology implements Serializable {
 
 
-
-
-
-
     public static void printMetrics(Nimbus.Client client, String name) throws Exception {
         ClusterSummary summary = client.getClusterInfo();
         String id = null;
-        for (TopologySummary ts: summary.get_topologies()) {
+        for (TopologySummary ts : summary.get_topologies()) {
             if (name.equals(ts.get_name())) {
                 id = ts.get_id();
             }
         }
         if (id == null) {
-            throw new Exception("Could not find a rich_topology named "+name);
+            throw new Exception("Could not find a rich_topology named " + name);
         }
         TopologyInfo info = client.getTopologyInfo(id);
         int uptime = info.get_uptime_secs();
         long acked = 0;
         long failed = 0;
         double weightedAvgTotal = 0.0;
-        for (ExecutorSummary exec: info.get_executors()) {
+        for (ExecutorSummary exec : info.get_executors()) {
             if ("spout".equals(exec.get_component_id())) {
                 SpoutStats stats = exec.get_stats().get_specific().get_spout();
                 Map<String, Long> failedMap = stats.get_failed().get(":all-time");
                 Map<String, Long> ackedMap = stats.get_acked().get(":all-time");
                 Map<String, Double> avgLatMap = stats.get_complete_ms_avg().get(":all-time");
-                for (String key: ackedMap.keySet()) {
+                for (String key : ackedMap.keySet()) {
                     if (failedMap != null) {
                         Long tmp = failedMap.get(key);
                         if (tmp != null) {
@@ -60,8 +55,8 @@ public class IeStormTopology implements Serializable {
                 }
             }
         }
-        double avgLatency = weightedAvgTotal/acked;
-        System.out.println("uptime: "+uptime+" acked: "+acked+" avgLatency: "+avgLatency+" acked/sec: "+(((double)acked)/uptime+" failed: "+failed));
+        double avgLatency = weightedAvgTotal / acked;
+        System.out.println("uptime: " + uptime + " acked: " + acked + " avgLatency: " + avgLatency + " acked/sec: " + (((double) acked) / uptime + " failed: " + failed));
     }
 
     public static void kill(Nimbus.Client client, String name) throws Exception {
@@ -70,7 +65,7 @@ public class IeStormTopology implements Serializable {
         client.killTopologyWithOpts(name, opts);
     }
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws Exception {
 
         try {
             Gate.setGateHome(new File("/opt/gate-8.3-build5704-ALL"));
@@ -92,15 +87,15 @@ public class IeStormTopology implements Serializable {
         builder.setSpout("Simulated_Tweet", new Simulated_Tweet_rich_Spout(), 1);
 
         builder.setBolt("Tokenizer_Bolt", new Tokenizer_rich_Bolt(), 1).shuffleGrouping("Simulated_Tweet");
-       // builder.setBolt("Gazetteer_Bolt", new Gazetteer_rich_Bolt(), 1).shuffleGrouping("Tokenizer_Bolt");
+        // builder.setBolt("Gazetteer_Bolt", new Gazetteer_rich_Bolt(), 1).shuffleGrouping("Tokenizer_Bolt");
         builder.setBolt("Annotation_Bolt", new Annotation_rich_Bolt(), 1).shuffleGrouping("Tokenizer_Bolt");
         builder.setBolt("Persist_Bolt", new Persist_rich_Bolt(), 1).shuffleGrouping("Annotation_Bolt");
 
         Config conf = new Config();
         conf.registerMetricsConsumer(org.apache.storm.metric.LoggingMetricsConsumer.class);
         conf.registerSerialization(TweetEvent.class);
-       // conf.put("tweetFile", "tweet50.txt");
-        conf.setDebug(true);
+        // conf.put("tweetFile", "tweet50.txt");
+        //conf.setDebug(true);
         conf.put("persist.file", "output50.txt");
 
 
@@ -110,43 +105,22 @@ public class IeStormTopology implements Serializable {
         }
 
         conf.setNumWorkers(1);
-        LocalCluster localCluster = new LocalCluster();
-        localCluster.submitTopology(name, conf, builder.createTopology());
-        /*try {
-            StormSubmitter.submitTopologyWithProgressBar(name, conf, builder.createTopology());
-        } catch (AlreadyAliveException e) {
-            e.printStackTrace();
-        } catch (InvalidTopologyException e) {
-            e.printStackTrace();
-        } catch (AuthorizationException e) {
-            e.printStackTrace();
-        }*/
-        Utils.sleep(100000);
-        localCluster.shutdown();
+        StormSubmitter.submitTopologyWithProgressBar(name, conf, builder.createTopology());
+
+        /*Utils.sleep(100000);
+        localCluster.shutdown();*/
 
 
-       /* Map clusterConf = Utils.readStormConfig();
+        Map clusterConf = Utils.readStormConfig();
         clusterConf.putAll(Utils.readCommandLineOpts());
-        Nimbus.Client client = NimbusClient.getConfiguredClient(clusterConf).getClient();*/
+        Nimbus.Client client = NimbusClient.getConfiguredClient(clusterConf).getClient();
 
 
         //Sleep for 5 mins
-        /*for (int i = 0; i < 10; i++) {
-            try {
-                Thread.sleep(30 * 1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            try {
-                printMetrics(client, name);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }*/
-        /*try {
-            kill(client, name);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
+        for (int i = 0; i < 50; i++) {
+            Thread.sleep(30 * 1000);
+            printMetrics(client, name);
+        }
+        kill(client, name);
     }
 }
